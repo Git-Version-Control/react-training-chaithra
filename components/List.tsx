d58803603a50fffcf4590dacabfe595622ec0466
Chaithra <102819533+Git-Version-Control@users.noreply.github.com>
2022-05-04 18:52:34 +0530

    Add files via upload

--- components/List.tsx
+++ components/List.tsx
@@ -1,7 +1,8 @@
-//Session-12
+import React from "react";
 import "./List.css";
 import Item from "./Item";
 import { StoryType } from "../types";
+import { useState } from "react";
 
 type ListProps = {
   listOfItems: Array<StoryType>;
@@ -35,35 +36,4 @@ const List = ({ listOfItems, onClickDelete }: ListProps) => {
   );
 };
 
-export default List;
-//Session-11
-/*import "./List.css";
-
-const List = ({ listOfItems }: any) => {
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>URL</th>
-            <th>Author</th>
-            <th>Comments</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listOfItems.map((item: any) => (
-            <tr key={item.objectID}>
-              <td className="itemTitle">{item.title}</td>
-              <td className="itemUrl">{item.url}</td>
-              <td>{item.author}</td>
-              <td>{item.num_comments}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default List;*/
+export default React.memo(List);
