a56fdeb3e77f20b7d0b633789d58d57fcea5e5b3
Chaithra <102819533+Git-Version-Control@users.noreply.github.com>
2022-05-22 18:26:22 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import InputWithLabel from "./components/InputWithLabel";
 import logo from "./assets/logo.png";
 import usePersistence from "./hooks/usePersistence";
 import React, {
+  useState,
   useEffect,
   useMemo,
   useReducer,
@@ -14,6 +15,7 @@ import axios from "axios";
 import { useDebounce } from "./hooks/useDebounce";
 import { StateType, StoryType, ActionType } from "./types";
 import { Link } from "react-router-dom";
+import Pagination from "./components/Pagination";
 
 export const title: string = "React Training";
 
@@ -35,8 +37,9 @@ export function storiesReducer(state: StateType, action: ActionType) {
   }
 }
 
-const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
-
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?hitsPerPage=10&query=";
+//const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?hitsPerPage=10&page=";
+//const API_ENDPOINT_PAGE = "https://hn.algolia.com/api/v1/search?page=";
 interface AppContextType {
   onClickDelete: (e: number) => void;
 }
@@ -46,12 +49,16 @@ export const AppContext = createContext<AppContextType | null>(null);
 function App(): JSX.Element {
   const [searchText, setSearchText] = usePersistence("searchTerm", "React");
   const debouncedUrl = useDebounce(API_ENDPOINT + searchText);
+  //let debouncedUrl = useDebounce(API_ENDPOINT + searchText);
 
   const [stories, dispatchStories] = useReducer(storiesReducer, {
     data: [],
     isError: false,
     isLoading: false,
   });
+  //const[post,setPosts]=useState([]);
+  const[currentPage, setCurrentPage]=useState(1);
+  const[itemPerPage, setItemPerPage]=useState(20);
 
   const sumOfComments = useMemo(
     () =>
@@ -66,15 +73,19 @@ function App(): JSX.Element {
     dispatchStories({ type: "INIT_FETCH" });
     try {
       const response = await axios.get(debouncedUrl);
+      //setItemPerPage(response.data);
+      //setSearchText(response.data);
+      //setPosts(response.data)
       dispatchStories({
         type: "SET_STORIES",
         payload: { data: response.data.hits },
       });
+      
     } catch {
       dispatchStories({ type: "FETCH_FAILURE" });
     }
   }, [debouncedUrl]);
-
+  console.log(stories.data.length);
   useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
@@ -86,7 +97,12 @@ function App(): JSX.Element {
   const handleDeleteClick = useCallback((objectId: number) => {
     console.log("Delete click captured", objectId);
     dispatchStories({ type: "REMOVE_STORY", payload: { id: objectId } });
+    setItemPerPage(prevCount=>prevCount-1);
   }, []);
+
+  /*function  handlePagination(){
+    debouncedUrl = useDebounce(API_ENDPOINT_PAGE + paginate);
+  }*/
 
   if (stories.isError) {
     return (
@@ -95,6 +111,12 @@ function App(): JSX.Element {
       </h1>
     );
   }
+  
+  const indexOfLastPage=currentPage* itemPerPage;
+  const indexOfFirstPage=indexOfLastPage-itemPerPage;
+  //console.log(indexOfFirstPage, indexOfLastPage);
+  const paginate =( pageNumber: React.SetStateAction<number>):any => setCurrentPage(pageNumber);
+  //const currentPageList=searchText.slice(indexOfFirstPage,indexOfLastPage);
 
   return (
     <div>
@@ -114,14 +136,17 @@ function App(): JSX.Element {
         <Link to="/login" state={{ id: "1234" }}>
           <h6>Login</h6>
         </Link>
+        <Pagination itemPerPage={itemPerPage} totalPosts={100} paginate={paginate}/>
       </nav>
       {stories.isLoading ? (
         <h1 style={{ marginTop: "10rem" }}>Loading</h1>
       ) : (
-        <AppContext.Provider value={{ onClickDelete: handleDeleteClick }}>
+        <AppContext.Provider value={{ onClickDelete: handleDeleteClick }}>          
           <List listOfItems={stories.data} />
         </AppContext.Provider>
       )}
+     {/*<Pagination itemPerPage={itemPerPage} totalPosts={100} onClick={handlePagination()}/>*/ }
+     {}
     </div>
   );
 }
